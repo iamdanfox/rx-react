7a03d852d46836ac87b519c5f843d0358c48d94f
Dan Fox <iamdanfox@gmail.com>
2016-02-08 22:43:18 +0000

    Mocha tests run in browser

---
+++ src/test.tsx
@@ -0,0 +1,9 @@
+/// <reference path="../typings/mocha/mocha.d.ts" />
+
+import { expect } from "chai";
+
+describe("HELLO", () => {
+  it("should do something", () => {
+    expect(true).to.be.true;
+  });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,36 @@
 var path = require('path');
 
-module.exports = {
-  entry: {
-    index: './src/index.tsx',
-  },
-  output: {
-    path: path.join(__dirname, 'build'),
-    filename: '[name].js'
-  },
-  module: {
-    loaders: [{
-      test: /\.tsx$/,
-      loaders: ['ts-loader'],
-      exclude: /node_modules/,
-      include: __dirname
-    }]
+module.exports = [{
+    entry: {
+      index: './src/index.tsx'
+    },
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: '[name].js'
+    },
+    module: {
+      loaders: [{
+        test: /\.tsx$/,
+        loaders: ['ts-loader'],
+        exclude: /node_modules/,
+        include: __dirname
+      }]
+    }
+  }, {
+    entry: {
+      test: 'mocha!./src/test.tsx'
+    },
+    output: {
+      path: path.join(__dirname, 'build'),
+      filename: '[name].js'
+    },
+    module: {
+      loaders: [{
+        test: /\.tsx$/,
+        loaders: ['ts-loader'],
+        exclude: /node_modules/,
+        include: __dirname
+      }]
+    }
   }
-};
+];
