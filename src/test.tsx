2dfe55e75760e5d412efaf578871635d2889b9b9
Dan Fox <iamdanfox@gmail.com>
2016-02-09 00:51:42 +0000

    Copy some testing ideas from RxJS spec

--- src/test.tsx
+++ src/test.tsx
@@ -1,9 +1,24 @@
 /// <reference path="../typings/mocha/mocha.d.ts" />
 
+import { Promise } from "es6-promise";
 import { expect } from "chai";
+import { Subject } from "rxjs";
+import { SomethingModel } from "./something.tsx";
 
-describe("HELLO", () => {
-  it("should do something", () => {
-    expect(true).to.be.true;
+describe("SomethingModel", () => {
+  it("should increase from 0 to 1 when an up event happens", (done) => {
+
+    let ups$ = new Subject();
+    const model = new SomethingModel(ups$, new Subject());
+    const expected = [{count: 0}, {count: 1}];
+
+    var result = model.state$.take(2).forEach(function (x) {
+      expect(x).to.deep.equal(expected.shift());
+    }, null, Promise)
+    .then(done);
+
+    ups$.next({});
+
+    expect(typeof result.then).to.equal('function');
   });
 });
